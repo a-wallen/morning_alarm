9aa039f5236f056c88658332e223a5a02bf64141
Stephen (Alex) Wallen <wallenstephe@seattleu.edu>
2020-01-30 20:49:42 -0800

    boilerplate

---
+++ lib/utils.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+// UI constants for the application i.e colors etc
+const primaryColor = const Color(0xFFFF8961);
+
+const primaryGradient = const LinearGradient(
+  colors: const [Color(0xFFFD60A3), Color(0xFFFF8961)],
+  begin: Alignment.topRight,
+  end: Alignment.bottomLeft,
+);
+
+// metrics
+
+// screen sizing metrics
+Size screenSize(BuildContext context) {
+  return MediaQuery.of(context).size;
+}
+
+double screenHeight(BuildContext context,
+    {double dividedBy = 1, double reducedBy = 0.0}) {
+  return (screenSize(context).height - reducedBy) / dividedBy;
+}
+
+double screenWidth(BuildContext context,
+    {double dividedBy = 1, double reducedBy = 0.0}) {
+  return (screenSize(context).width - reducedBy) / dividedBy;
+}
+
+// time
+
