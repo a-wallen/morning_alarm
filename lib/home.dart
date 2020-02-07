e49000d77ceb3806b690c497217f12b033a47e46
Ethan G <guttmane@seattleu.edu>
2020-02-06 20:39:59 -0800

    Alarm Work Started

--- lib/home.dart
+++ lib/home.dart
@@ -21,19 +21,62 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Timer timer;
   DateTime now = DateTime.now();
+  //DateTime alarmTest;
   int alarmMinutes=30, alarmHours=7; // alarm target times
   String date, hours, minutes, seconds; // current time
+  FlutterLocalNotificationsPlugin alarmNotificationPlugin; //alarm
+
   void initState() {
     super.initState();
+
+    //alarm notification plugin initialization
+    var initializationSettingsAndroid =
+      new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(initializationSettingsAndroid, initializationSettingsIOS);
+    alarmNotificationPlugin = new FlutterLocalNotificationsPlugin();
+    alarmNotificationPlugin.initialize(initializationSettings);
+
+
     Timer.periodic(Duration(seconds: 1), (Timer t) {
       setState(() {
         now = DateTime.now();
+        //alarmTest = DateTime(now.year, now.month, now.day, now.hour, now.minute+1);
         date = DateFormat('EEE d MMM').format(now);
         seconds = DateFormat('ss').format(now);
         hours = DateFormat('kk').format(now);
         minutes = DateFormat('mm').format(now);
       });
     });
+  }
+
+  void _setAlarm() async{
+    //Currently doesn't work with this code to schedule alarm ten seconds in advance
+    //commented out code by itself works to make it send a notification as soon as function is called
+    var alarmTime = DateTime.now().add(new Duration(seconds: 10));
+    var androidPlatformChannelSpecifics =
+    new AndroidNotificationDetails('your other channel id',
+        'your other channel name', 'your other channel description');
+    var iOSPlatformChannelSpecifics =
+    new IOSNotificationDetails();
+    NotificationDetails platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await alarmNotificationPlugin.schedule(0,
+    'scheduled title',
+    'scheduled body',
+    alarmTime,
+    platformChannelSpecifics);
+
+    /*showDialog(
+      context: context,
+      builder: (_) {
+        return new AlertDialog(
+          title: Text("PayLoad"),
+          content: Text("Payload : lol"),
+        );
+      },
+    );*/
+
   }
 
   @override
@@ -75,7 +118,7 @@ class _HomePageState extends State<HomePage> {
                   Icons.alarm,
                   color: SECOND_COLOR,
                 ),
-                onPressed: null,
+                onPressed: _setAlarm,
               ),
 //              Text(
 //                "Alarm at: $alarmHours:$alarmMinutes",
